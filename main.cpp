ebb76d79e85d9cae6f823b501c4313c05d16a05a
sagarmunjal <munjal.sagar@gmail.com>
2024-09-24 20:14:49 +0530

    added esp32 as access point

--- main.cpp
+++ main.cpp
@@ -4,10 +4,11 @@
 //Declarations
 const int builtinLED = 2;
 
-  // Replace these with your credentials
-  const char* ssid = "Airtel_Virus.2o";
-  const char* password = "$h@lly@2024";
+  const char* ssid = "Airtel_Virus.2o";     // WiFi SSID
+  const char* password = "$h@lly@2024";     // WiFi Password
 
+  const char* apSSID = "ESP32-AP";         // Access Point SSID
+  const char* apPassword = "123456789";    // Access Point Password
 
 void setup() {
   // put your setup code here, to run once:
@@ -17,12 +18,17 @@ void setup() {
 
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi:" + String(ssid));
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("Connected to Wi-Fi");
+  Serial.println("Connected to WiFi:" + String(ssid));
+  Serial.print("IP address: " + String(WiFi.localIP()));
 
+  // Start the Access Point
+  WiFi.softAP(apSSID, apPassword);
+  Serial.print("Access Point:" + String(apSSID) + "started");
 }
 
 void loop() {
@@ -33,5 +39,4 @@ void loop() {
     digitalWrite(builtinLED, LOW);
     delay(500);
   }
-
 }

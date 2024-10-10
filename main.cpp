42825ae39221cdf62fe17ead2bd707fde0e7c95b
sagarmunjal <munjal.sagar@gmail.com>
2024-10-10 14:11:00 +0530

    Create main.cpp
    This is the bare shell code structure and the starting line for the main.cpp
    Familiarize yourself with the structure hence you start to understand and relate which program logic will belong to which section. 
    We have two sections to begin with. 
    Setup - program logic that stages the esp32 
    Loop -  Program logic that runs repeatedly (can be for inputting, receiving, outputting, transmitting data, continuously )
    
    Once we understand this logic. We can start our journey exploring microcontrollers..

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//Libraries included
+//#include <Arduino.h>
+
+
+void setup() {
+  //setup asks for the code that stages the microcontroller (esp32-devkit-espressif) 
+  Serial.begin(115200);
+  Serial.println("Booting....");
+}
+
+void loop(){
+  // loop contains the program logic code that runs tasks, jobs, inputs, outputs repeatedly
+}
+
